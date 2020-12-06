1af62dbfc8b0aab85560c827b7cd7841d2bcb1c7
James Edmonston <james@ethercreative.co.uk>
2020-12-06 03:38:22 +0000

    Add default Docusaurus favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "assets/favicon.ico",
   organizationName: "jamesedmonston",
   projectName: "graphql-authentication-docs",
   themeConfig: {
